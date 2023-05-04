45d6170c8c64020279fe72e6bb2f498152436e59
KidooSAma <tallesbraga63@gmail.com>
2023-05-04 13:16:37 -0300

    Add PHP fIles

---
+++ php/patient.php
@@ -0,0 +1,60 @@
+<?
+require_once "person.php";
+class Patient extends Person{
+    protected $date_surgery;
+    protected $medical_history;
+    protected $expenses;
+    protected $type_surgery;
+    protected $doctor;
+    protected $post_surgery_statuses;
+    protected $insurance;
+
+    public function setDateSurgery($dts){
+        $this->date_surgery = $dts;
+    }
+    public function setMedicalHistory($mh){
+        $this->medical_history = $mh;
+    }
+    public function setExpenses($exp){
+        $this->expenses = $exp;
+    }
+    public function setTypeSurgery($mh){
+        $this->type_surgery = $mh;
+    }
+    public function setDoctor($dct){
+        $this->doctor = $dct;
+    }
+    public function setPostSurgeryStatuses($pss){
+        $this->post_surgery_statuses = $pss;
+    }
+    public function setInsurance($ins){
+        $this->insurance = $ins;
+    }
+
+
+    public function getDateSurgery(){
+        return $this->date_surgery;
+    }
+    public function getMedicalHistory(){
+        return $this->medical_history;
+    }
+    public function getExpenses(){
+        return $this->expenses;
+    }
+    public function getTypeSurgery(){
+        return $this->type_surgery;
+    }
+    public function getDoctor(){
+        return $this->doctor;
+    }
+    public function getPostSurgeryStatuses(){
+        return $this->post_surgery_statuses;
+    }
+    public function getInsurance(){
+        return $this->insurance;
+    }
+}
+
+
+
+?>
---
+++ php/room.php
@@ -0,0 +1,12 @@
+<?
+
+class Room{
+    protected $name;
+    protected $location;
+    protected $type_surgeries;
+    protected $description;
+}
+
+
+
+?>
