6169919e8baccf07f54031bdf0d34a991de2ee50
KidooSama <tallesbraga63@gmail.com>
2023-05-30 16:51:03 -0300

    teste

--- php/surgeryDAO.php
+++ php/surgeryDAO.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'Conexao.php';
 require_once 'surgery.php';   
-class SuergeryDAO {
+class SurgeryDAO {
 
     public function create (Surgery $surgery) {
         $sql = 'INSERT INTO  surgery (name, description) VALUES (?,?)';
@@ -12,49 +12,49 @@ class SuergeryDAO {
         $surgery->setId(Conexao::getConn()->lastInsertId());
     }
 
-    public function read(){
-        $sql = 'SELECT * FROM doctor';
-        $stmt = Conexao::getConn()->prepare($sql);
-        $stmt->execute();
-        $doctors = [];
-        if($stmt->rowCount() > 0){
-            $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            foreach ($resultado as $row) {
-                $doctor = new Doctor($row['name'],$row['speciality'],$row['gender'],$row['crm'], $row['number'], $row['cpf'], $row['date'], $row['adress'] );
-                $doctors[] = $doctor;
-            }
-        }
-        return $doctors;
-    }
+//     public function read(){
+//         $sql = 'SELECT * FROM doctor';
+//         $stmt = Conexao::getConn()->prepare($sql);
+//         $stmt->execute();
+//         $doctors = [];
+//         if($stmt->rowCount() > 0){
+//             $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+//             foreach ($resultado as $row) {
+//                 $doctor = new Doctor($row['name'],$row['speciality'],$row['gender'],$row['crm'], $row['number'], $row['cpf'], $row['date'], $row['adress'] );
+//                 $doctors[] = $doctor;
+//             }
+//         }
+//         return $doctors;
+//     }
     
 
-    public function update(Doctor $doctor) {
-    $sql = 'UPDATE doctor SET name=?, speciality=?, gender=?, crm=?, number=?, cpf=?, date=?, adress=? WHERE id=?';
-    $stmt = Conexao::getConn()->prepare($sql);
-    $stmt->bindValue(1, $doctor->getName());
-    $stmt->bindValue(2, $doctor->getSpeciality());
-    $stmt->bindValue(3, $doctor->getGen());
-    $stmt->bindValue(4, $doctor->getCrm());
-    $stmt->bindValue(5, $doctor->getNum());
-    $stmt->bindValue(6, $doctor->getCpf());
-    $stmt->bindValue(7, $doctor->getDate());
-    $stmt->bindValue(8, $doctor->getAdr());
-    $stmt->bindValue(9, $doctor->getId());
-    $stmt->execute();
-}
+//     public function update(Doctor $doctor) {
+//         $sql = 'UPDATE doctor SET name=?, speciality=?, gender=?, crm=?, number=?, cpf=?, date=?, adress=? WHERE id=?';
+//         $stmt = Conexao::getConn()->prepare($sql);
+//         $stmt->bindValue(1, $doctor->getName());
+//         $stmt->bindValue(2, $doctor->getSpeciality());
+//         $stmt->bindValue(3, $doctor->getGen());
+//         $stmt->bindValue(4, $doctor->getCrm());
+//         $stmt->bindValue(5, $doctor->getNum());
+//         $stmt->bindValue(6, $doctor->getCpf());
+//         $stmt->bindValue(7, $doctor->getDate());
+//         $stmt->bindValue(8, $doctor->getAdr());
+//         $stmt->bindValue(9, $doctor->getId());
+//         $stmt->execute();
+// }
 
-public function getById($id) {
-    $sql = 'SELECT * FROM doctor WHERE id=?';
-    $stmt = Conexao::getConn()->prepare($sql);
-    $stmt->bindValue(1, $id);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($result) {
-        return new Doctor($result['name'], $result['speciality'], $result['gender'], $result['crm'], $result['number'], $result['cpf'], $result['date'], $result['adress']);
-    } else {
-        return null;
-    }
-}
+// public function getById($id) {
+//     $sql = 'SELECT * FROM doctor WHERE id=?';
+//     $stmt = Conexao::getConn()->prepare($sql);
+//     $stmt->bindValue(1, $id);
+//     $stmt->execute();
+//     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//     if ($result) {
+//         return new Doctor($result['name'], $result['speciality'], $result['gender'], $result['crm'], $result['number'], $result['cpf'], $result['date'], $result['adress']);
+//     } else {
+//         return null;
+//     }
+// }
 
 
     public function delete($id) {
